cd00d687982e52e9b699e7b1dc7707a4657c0b3f
Atobaum <atobaum@gmail.com>
2020-07-14 11:21:21 +0900

    chore: Setup html webpack plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackHarddiskPlugin = require("html-webpack-harddisk-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -38,9 +41,19 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ["dist"],
+    }),
     new MiniCssExtractPlugin({
       filename: "style.css",
     }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./public/index.html"),
+      filename: path.resolve(__dirname, "./dist/index.html"),
+      inject: true, // inject built script in the end of body tag
+      alwaysWriteToDisk: true,
+    }),
+    new HtmlWebpackHarddiskPlugin(),
   ],
   devServer: {
     contentBase: path.join(__dirname, "./dist"),
