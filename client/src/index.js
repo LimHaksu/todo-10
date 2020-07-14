67848e5822b0947a821fba92d9db989a4da95990
Atobaum <atobaum@gmail.com>
2020-07-14 17:59:58 +0900

    feat: Add simple structure to index page

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,21 @@
-const t = () => console.log(1);
+import "./reset.css";
+import Button from "./components/Button";
+import Header from "./components/Header";
+import List from "./components/List";
+import Element from "./components/Element";
+
+const app = new Element("div");
+document.getElementById("app").appendChild(app.$el);
+const b1 = new Button();
+
+app.appendChild(new Header());
+app.appendChild(b1);
+
+const list = new List();
+app.appendChild(list);
+
+[1, 2, 3, 4].forEach((n) => {
+  const li = document.createElement("li");
+  li.innerText = n;
+  list.appendChild({ $el: li });
+});
