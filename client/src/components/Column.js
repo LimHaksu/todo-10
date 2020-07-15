e4fd70bcc05d07aaf4262e2ac0b9776ec53fa1e1
Atobaum <atobaum@gmail.com>
2020-07-15 17:19:29 +0900

    feat: Make column markup #34

--- client/src/components/Column.js
+++ client/src/components/Column.js
@@ -1,19 +1,79 @@
-import { List, Element } from "./basic";
+import { List, Element, H } from "./basic";
 import Todo from "./Todo";
-import "./Column.css";
+import "./Column.scss";
+import Button from "./basic/Button";
 
 export default class Column extends Element {
-  constructor() {
+  /**
+   *
+   * @param {string} title
+   * @param {Todo[]} todos
+   */
+  constructor(title, todos = []) {
     super("div", { class: "column" });
-    this.appendChild(new Element("h1", { text: "Pending" }));
+    const header = new Element("div", { class: "column-header" });
+
+    const headerLeft = new Element("div", { class: "flex" });
+    this.$count = new Element("div", {
+      text: "" + todos.length,
+      class: "column-todo-count",
+    });
+    headerLeft.appendChild(this.$count);
+    this.$title = new H(2, title);
+    headerLeft.appendChild(this.$title);
+
+    const headerRight = new Element("div");
+    this.$newBtn = new Button(
+      "+",
+      (evt) => {
+        alert("+ button clicked");
+      },
+      { class: ["reset-button-style"] }
+    );
+    headerRight.appendChild(this.$newBtn);
+    this.$dotBtn = new Button(
+      "...",
+      (evt) => {
+        alert("... button clicked");
+      },
+      { class: ["reset-button-style"] }
+    );
+    headerRight.appendChild(this.$dotBtn);
+
+    header.appendChild(headerLeft);
+    header.appendChild(headerRight);
+    this.appendChild(header);
+
     this.$todos = new List(true);
     this.appendChild(this.$todos);
-    this.$todos.push(1, new Todo());
-    this.$todos.push(2, new Todo());
-    this.$todos.push(3, new Todo());
-    this.$todos.push(4, new Todo());
-    this.$todos.push(5, new Todo());
-    this.$todos.push(6, new Todo());
-    this.$todos.push(7, new Todo());
+
+    todos.forEach(([i, todo]) => {
+      this.addTodo(i, todo);
+    });
+  }
+
+  /**
+   * @private
+   */
+  refreshCount() {
+    this.$count.setText(this.$todos.getCount());
+  }
+
+  addTodo(key, todo) {
+    this.$todos.push(key, todo);
+    this.refreshCount();
+  }
+
+  removeTodo(key) {
+    this.$todos.remove(key);
+    this.refreshCount();
+  }
+
+  /**
+   *
+   * @param {string} title
+   */
+  setTitle(title) {
+    this.$title.setText(title);
   }
 }
