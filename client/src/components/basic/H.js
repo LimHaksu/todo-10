76ff5dca8f6d9faa32e7173e825db89bc78d150a
Atobaum <atobaum@gmail.com>
2020-07-15 15:43:36 +0900

    feat: New basic element H, represents h1-h6

---
+++ client/src/components/basic/H.js
@@ -0,0 +1,10 @@
+import Element from "./Element";
+
+export default class H extends Element {
+  constructor(level, text, option) {
+    if (!level) throw new Error("Specify level");
+    if (typeof level !== "number" || !(1 <= level && level <= 6))
+      throw new Error("Level should be >=1, <=6");
+    super("h" + level, { ...option, text });
+  }
+}
