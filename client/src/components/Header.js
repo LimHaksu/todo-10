d98bd5e0cc2af8577bfe3d29f42a5f52be701367
Atobaum <atobaum@gmail.com>
2020-07-15 15:44:01 +0900

    feat: Make header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,11 @@
 import Element from "./basic/Element";
-import "./Header.css";
+import "./Header.scss";
+import H from "./basic/H";
 
 export default class Header extends Element {
   constructor() {
     super("header", { class: ["header"] });
+    this.appendChild(new H(1, "TODO 서비스", { class: "logo" }));
+    this.appendChild(new H(2, "Menu"));
   }
 }
