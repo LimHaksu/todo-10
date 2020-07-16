14573cdcfd2a1eddd51b0076c105b6d46eaad5ae
LimHaksu <limhaksu@naver.com>
2020-07-16 12:56:13 +0900

    feat: 서버 테스트 환경 세팅
    npm test 커맨드 입력시 jest 테스트 실행

---
+++ server/routes/__test__/index.test.js
@@ -0,0 +1,13 @@
+import request from "supertest";
+import app from "../../app";
+
+describe("Test /", () => {
+  it("should return world!", (done) => {
+    request(app)
+      .get("/")
+      .then((response) => {
+        expect(response.text).toBe(`{"title":"Express"}`);
+        done();
+      });
+  });
+});
