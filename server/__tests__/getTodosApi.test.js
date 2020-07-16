be48c1e438daf089504a1a0c6a3ea268534391b8
Atobaum <atobaum@gmail.com>
2020-07-16 13:01:24 +0900

    test: add get todos api test

--- server/__tests__/getTodosApi.test.js
+++ server/__tests__/getTodosApi.test.js
@@ -3,12 +3,36 @@ const app = require("../app");
 
 describe("GET /api/todos", () => {
   const server = request(app);
-  it("gives todos", (done) => {
+  it("requires column_id", (done) => {
     server
       .get("/api/todos")
       .expect("Content-Type", /json/)
-      .expect(200)
+      .expect(400)
       .end((err, res) => {
+        if (err) return done(err);
+        if (res.body) {
+          expect(res.body.error).toBeTruthy();
+          return done();
+        }
+      });
+  });
+
+  it("gives todos", (done) => {
+    server
+      .get("/api/todos?columns=1")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect({
+        result: {
+          column_id: 1,
+          todos: [
+            { id: 1, username: "user1", content: "todo1" },
+            { id: 2, username: "user1", content: "todo2" },
+          ],
+        },
+      })
+      .end((err, res) => {
+        if (err) return done(err);
         if (res.body) return done();
       });
   });
