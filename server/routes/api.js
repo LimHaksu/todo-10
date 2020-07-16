ff8e4449bcf1f99ea8a62ce6dae2ac64966e457a
Atobaum <atobaum@gmail.com>
2020-07-16 13:19:00 +0900

    feat: Implement GET /api/todos

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,8 +3,31 @@ const { Router } = require("express");
 const router = Router();
 
 /* GET home page. */
-router.get("/todos", function (req, res, next) {
-  res.send("asdf");
+router.get("/todos", (req, res) => {
+  if (!req.query.columns) {
+    res.status(400);
+    res.json({
+      error: "Specify columns",
+    });
+  }
+
+  const columns = req.query.columns.split(",").map(parseInt);
+
+  const result = columns
+    .map((column_id) => {
+      if (column_id === 1) {
+        return {
+          column_id,
+          todos: [
+            { id: 1, username: "user1", content: "todo1" },
+            { id: 2, username: "user1", content: "todo2" },
+          ],
+        };
+      } else return null;
+    })
+    .filter((data) => data !== null);
+  res.json({ result });
+});
 });
 
 module.exports = router;
