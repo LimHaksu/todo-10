204db24bd3c66d48d36391b97ff65b83d53d9713
LimHaksu <limhaksu@naver.com>
2020-07-17 13:29:53 +0900

    feat: ModalColumnTitleEdit 컴포넌트 작성

--- client/src/components/Column/index.js
+++ client/src/components/Column/index.js
@@ -3,9 +3,13 @@ import ModalColumnTitleEdit from "../ModalColumnTitleEdit";
 import NewTodoForm from "./NewTodoForm";
 import "./Column.scss";
 
+const onTitleEdit = () => {
+  alert("제목 수정 버튼 클릭");
+};
+
 const handleColumnTitleDoubleClick = (event, columnTitle) => {
   event.preventDefault();
-  new ModalColumnTitleEdit(columnTitle);
+  new ModalColumnTitleEdit({ id: 1, columnTitle, onTitleEdit });
 };
 
 export default class Column extends Element {
