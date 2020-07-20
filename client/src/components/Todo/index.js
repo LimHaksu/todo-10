b6d87c160184099428badc4d19752602347b8b81
Atobaum <atobaum@gmail.com>
2020-07-20 15:49:47 +0900

    feat: Call api when remove todo

--- client/src/components/Todo/index.js
+++ client/src/components/Todo/index.js
@@ -3,6 +3,7 @@ import "./Todo.css";
 import Button from "../basic/Button";
 import ModalTodoEdit from "../ModalTodoEdit";
 import LogEvent from "../../lib/LogEvent";
+import api from "../../lib/apiCallWrapper";
 
 export default class Todo extends Element {
   constructor({ id, content, username }) {
@@ -31,13 +32,15 @@ export default class Todo extends Element {
     deleteDiv.appendChild(
       new Button(
         "🗑️",
-        function () {
-          // TODO: API call
-
+        async function () {
+          const res = await api.removeTodoApi(id);
+          const result = res.result;
           const logEvent = new LogEvent("remove-todo", {
-            todoId: id,
-            todoContent: content,
-            username,
+            logId: result.log_id,
+            todoId: result.todo_id,
+            todoContent: result.todo_content,
+            columnContent: result.column_content,
+            username: result.username,
           });
           this.getDom().dispatchEvent(logEvent);
           this.removeSelf();
