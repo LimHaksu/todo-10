53857351fe14913ac24d85bee2fd041a201d0478
Atobaum <atobaum@gmail.com>
2020-07-20 17:19:44 +0900

    feat: response mocked todos

--- server/routes/apis/loadTodosApi.js
+++ server/routes/apis/loadTodosApi.js
@@ -1,25 +1,81 @@
 export default (req, res) => {
-  if (!req.query.columns) {
-    res.status(400);
-    res.json({
-      error: "Specify columns",
-    });
-  }
-
-  const columns = req.query.columns.split(",").map(parseInt);
-
-  const result = columns
-    .map((column_id) => {
-      if (column_id === 1) {
-        return {
-          column_id,
-          todos: [
-            { id: 1, username: "user1", content: "todo1" },
-            { id: 2, username: "user1", content: "todo2" },
-          ],
-        };
-      } else return null;
-    })
-    .filter((data) => data !== null);
+  const username = "circle";
+  let result = [];
+  result.push({
+    id: 1,
+    idx: 1,
+    title: "Todos",
+    username,
+    prev_column_id: 0,
+    todo_list: [
+      {
+        id: 1,
+        username,
+        column_id: 1,
+        prev_todo_id: null,
+        content: "todo1",
+        created_at: "now",
+        updated_at: "tom",
+      },
+      {
+        id: 2,
+        username,
+        column_id: 1,
+        prev_todo_id: null,
+        content: "todo2",
+        created_at: "now",
+        updated_at: "tom",
+      },
+      {
+        id: 3,
+        username,
+        column_id: 1,
+        prev_todo_id: null,
+        content: "todo3",
+        created_at: "now",
+        updated_at: "tom",
+      },
+    ],
+    created_at: "now",
+    updated_at: "tow",
+  });
+  result.push({
+    id: 2,
+    idx: 2,
+    title: "Doing",
+    username,
+    prev_column_id: 1,
+    todo_list: [
+      {
+        id: 4,
+        username,
+        column_id: 2,
+        prev_todo_id: null,
+        content: "todo11",
+        created_at: "now",
+        updated_at: "tom",
+      },
+      {
+        id: 5,
+        username,
+        column_id: 2,
+        prev_todo_id: null,
+        content: "todo21",
+        created_at: "now",
+        updated_at: "tom",
+      },
+      {
+        id: 6,
+        username,
+        column_id: 2,
+        prev_todo_id: null,
+        content: "todo32",
+        created_at: "now",
+        updated_at: "tom",
+      },
+    ],
+    created_at: "now",
+    updated_at: "tow",
+  });
   res.json({ result });
 };
