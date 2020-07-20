59c0053bd8969e9537d108e74b3333460f2c354d
Atobaum <atobaum@gmail.com>
2020-07-20 15:06:12 +0900

    feat: Create DELETE /api/todo
    - Check wheather todo_id is number;
    - If todo id is number, response mocked data

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,4 +8,21 @@ const router = Router();
 router.get("/todos", todosApi);
 router.get("/columns", columnsApi);
 
+router.delete("/todo", (req, res) => {
+  console.log(req.query);
+  const id = parseInt(req.query.todo_id);
+  if (isNaN(id)) {
+    res.status = 400;
+    res.send({ error: "Invalid todo id" });
+  } else {
+    res.json({
+      result: {
+        log_id: ~~(Math.random() * 1000),
+        todo_id: 1,
+        todo_content: "test toto sontent",
+        column_content: "column",
+      },
+    });
+  }
+});
 module.exports = router;
