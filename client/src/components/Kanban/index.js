268a466cfb45e0da62ccae4285afa215be05fad0
Atobaum <atobaum@gmail.com>
2020-07-21 10:35:08 +0900

    feat: Load todos from server and set when kanban is initialized

--- client/src/components/Kanban/index.js
+++ client/src/components/Kanban/index.js
@@ -1,8 +1,32 @@
 import "./Kanban.css";
 import List from "../basic/List";
+import Column from "../Column";
+import Todo from "../Todo";
+import api from "../../lib/apiCallWrapper";
 
 export default class Kanban extends List {
   constructor() {
     super(true, { class: "kanban" });
+    const createTestTodos = () => {
+      return [1, 2, 3, 4, 5, 6, 7].map((i) => [
+        i,
+        new Todo({ id: i, content: "todo " + i, userneme: "user " + i }),
+      ]);
+    };
+    const { loadTodoApi } = api;
+    loadTodoApi().then((res) => {
+      res.forEach((colData) => {
+        const todos = colData.todo_list.map((todoData) => [
+          todoData.id,
+          new Todo({
+            id: todoData.id,
+            content: todoData.content,
+            username: todoData.username,
+          }),
+        ]);
+        const column = new Column(colData.content, todos);
+        this.push(colData.id, column);
+      });
+    });
   }
 }
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -31,19 +31,6 @@ export default class Main extends Element {
 
     const kanban = new Kanban();
 
-    const createTestTodos = () => {
-      return [1, 2, 3, 4, 5, 6, 7].map((i) => [
-        i,
-        new Todo({ id: i, content: "todo " + i, userneme: "user " + i }),
-      ]);
-    };
-
-    kanban.push(1, new Column("Todo", createTestTodos()));
-    kanban.push(2, new Column("Doing", createTestTodos()));
-    kanban.push(3, new Column("Done", createTestTodos()));
-    kanban.push(4, new Column("Todo", createTestTodos()));
-    kanban.push(5, new Column("Doing", createTestTodos()));
-    kanban.push(6, new Column("Done", createTestTodos()));
     this.appendChild(kanban);
 
     const menu = new Menu();
