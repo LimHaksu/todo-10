10cf2c6630a1847550a43b273dca7bee3ca73819
Atobaum <atobaum@gmail.com>
2020-07-21 17:46:29 +0900

    feat: todo 추가하는 api 구현 #19
    - 기본적인 인풋 검사
    - 컬럼 있는지 검사
    - 추가하고 로그 정보 리턴
    - db에 로그 추가하기 구현 필요

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,19 @@
 import { Router } from "express";
 import todosApi from "./apis/loadTodosApi";
 import columnsApi from "./apis/columnsApi";
+import addTodoApi from "./apis/addTodoApi";
 
 const router = Router();
 
 /* GET home page. */
 router.get("/todos", todosApi);
 router.get("/columns", columnsApi);
+router.post("/todo", addTodoApi);
 
 router.delete("/todo", (req, res) => {
   const id = parseInt(req.query.todo_id);
   if (isNaN(id)) {
-    res.status = 400;
+    res.status(400);
     res.send({ error: "Invalid todo id" });
   } else {
     // TODO: db에서 todo 찾기
