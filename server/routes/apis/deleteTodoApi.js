eae9f20338eae84a91ce6c6560a3f28e294ac1b5
Atobaum <atobaum@gmail.com>
2020-07-21 23:27:01 +0900

    feat: db에서 투두 삭제 구현
    delete todo에서 투두가 있는지 확인하고 있으면 삭제합니다.
    로그 추가 구현이 필요합니다.

--- server/routes/apis/deleteTodoApi.js
+++ server/routes/apis/deleteTodoApi.js
@@ -1,20 +1,44 @@
-export default async (req, res) => {
+import useDbConnection from "../../lib/useDbConnection";
+
+const loadTodoSql = `
+SELECT col.content as col_content, 
+todo.content as todo_content,
+user.username as username
+FROM todo 
+JOIN todo_column as col ON todo.column_id=col.id 
+JOIN user ON todo.user_id=user.id 
+WHERE todo.id=?
+`;
+export default (req, res) => {
   const id = parseInt(req.query.todo_id);
   if (isNaN(id)) {
     res.status(400);
     res.send({ error: "Invalid todo id" });
   } else {
-    // TODO: db에서 todo 찾기
-    // TODO: db log에 로그 생성
-    // TODO: db에서 todo 삭제
-    res.json({
-      result: {
-        log_id: ~~(Math.random() * 1000),
-        todo_id: 1,
-        todo_content: "test toto sontent",
-        column_content: "column",
-        username: "crog",
-      },
+    useDbConnection(async (conn) => {
+      // TODO: db에서 todo 찾기
+
+      let [rows] = await conn.query(loadTodoSql, [id]);
+      if (rows.length === 0) {
+        res.status(400);
+        res.send({ error: "Invalid todo id" });
+      } else {
+        // TODO: db log에 로그 생성
+        const todoContent = rows[0].todo_content;
+        const colContent = rows[0].col_content;
+        const username = rows[0].username;
+        await conn.query("DELETE FROM todo WHERE id=?", [id]);
+
+        res.json({
+          result: {
+            log_id: ~~(Math.random() * 1000),
+            todo_id: id,
+            todo_content: todoContent,
+            column_content: colContent,
+            username: username,
+          },
+        });
+      }
     });
   }
 };
