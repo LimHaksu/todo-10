1e647b7b0afb9d49c067a50b8b2ccc295679512c
Atobaum <atobaum@gmail.com>
2020-07-22 13:02:40 +0900

    fix: Edit column title api can handle user error

--- server/routes/apis/columnsApi.js
+++ server/routes/apis/columnsApi.js
@@ -1,4 +1,5 @@
 import useDbConnection from "../../lib/useDbConnection";
+import getUserById from "../../lib/getUserById";
 
 const selectQuery = `select content from todo_column where id=?`;
 const updateQuery = `update todo_column set content=? where id=?`;
@@ -13,8 +14,20 @@ const isValid = (columnId, nextColumnContent) =>
 const modifyColumnContent = (req, res) => {
   const columnId = req.body.column_id;
   const nextColumnContent = req.body.next_column_content;
+
+  //TODO remove hardcoded user id
+  const userId = 1;
+
   if (isValid(columnId, nextColumnContent)) {
     useDbConnection(async (con) => {
+      const user = await getUserById(con, userId);
+      if (!user) {
+        res.status(401);
+        res.json({
+          error: "Invalid user",
+        });
+      }
+
       const [[selectResult]] = await con.query(selectQuery, [columnId]);
       if (!selectResult) {
         res.status(400);
@@ -29,9 +42,10 @@ const modifyColumnContent = (req, res) => {
       ]);
       res.json({
         result: {
-          log_id: 361,
+          log_id: ~~(Math.random() * 1000),
           prev_column_content: prevColumnContent,
           next_column_content: nextColumnContent,
+          username: user.username,
         },
       });
     });
