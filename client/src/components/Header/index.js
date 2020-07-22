43e01d8560a6f61ca86849fccd6d68583d872198
LimHaksu <limhaksu@naver.com>
2020-07-22 21:04:20 +0900

    feat: header에서 메뉴 버튼 클릭하면 show-menu 이벤트 발생

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -9,7 +9,9 @@ export default class Header extends Element {
     this.appendChild(new H(1, "TODO 서비스", { class: "logo" }));
     this.appendChild(
       new Button(new H(2, "Menu"), (evt) => {
-        alert("Menu button clicked");
+        this.getDom().dispatchEvent(
+          new CustomEvent("show-menu", { bubbles: true })
+        );
       })
     );
   }
