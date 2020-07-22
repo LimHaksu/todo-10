116dcadfdf22561893fb3b8f0aa68b4539978c4e
LimHaksu <limhaksu@naver.com>
2020-07-22 16:32:57 +0900

    feat: todo 추가시 로그를 DB에 저장함

--- server/routes/apis/addTodoApi.js
+++ server/routes/apis/addTodoApi.js
@@ -11,7 +11,16 @@ const addTodoSql = `
 insert into todo (user_id, column_id, idx, content) values(?, ?, 1, ?);
 `;
 
+const insertTodoAddLogSql = `
+INSERT INTO log 
+(user_id, action_type, data)
+VALUES (?, ?, ?)
+`;
+
 export default (req, res) => {
+  // TODO: remove hardcoded user id
+  const userId = 1;
+
   const { column_id, content } = req.body;
   // TODO fix hardcoded user id;
   const user_id = 1;
@@ -32,16 +41,28 @@ export default (req, res) => {
       }
       let [rows] = await conn.query(selectColumnByIdSql, [column_id]);
       if (rows.length == 0) throw new Error("Column isn't exist.");
-      const column_content = rows[0].content;
+      const columnContent = rows[0].content;
+
       await conn.query(updateTodoIdxSql, [column_id]);
       [rows] = await conn.query(addTodoSql, [user_id, column_id, content]);
+      const data = {
+        todoContent: content,
+        columnContent,
+      };
+
+      conn.query(insertTodoAddLogSql, [
+        userId,
+        "todo_add",
+        JSON.stringify(data),
+      ]);
+
       // TODO: save log
       res.json({
         result: {
           log_id: ~~(Math.random() * 1000),
           todo_id: rows.insertId,
           todo_content: content,
-          column_content,
+          column_content: columnContent,
           username: user.username,
         },
       });
