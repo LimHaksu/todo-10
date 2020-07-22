788f32e8dd8d4813953dc226e2c458041af19dc9
Atobaum <atobaum@gmail.com>
2020-07-22 10:08:10 +0900

    fix not operating function
    - isValid 함수 오타 수정
    - 같은 이름으로 수정할 때 에러 반환 수정

--- server/routes/apis/columnsApi.js
+++ server/routes/apis/columnsApi.js
@@ -3,39 +3,37 @@ import useDbConnection from "../../lib/useDbConnection";
 const selectQuery = `select content from todo_column where id=?`;
 const updateQuery = `update todo_column set content=? where id=?`;
 
-const isValid = (columnId, nextColumnContent) => {
-  return (
-    columnId &&
-    typeof columnId === "number" &&
-    content &&
-    typeof content === string &&
-    content.length > 0
-  );
-};
+const isValid = (columnId, nextColumnContent) =>
+  columnId &&
+  typeof columnId === "number" &&
+  nextColumnContent &&
+  typeof nextColumnContent === "string" &&
+  nextColumnContent.length > 0;
 
 const modifyColumnContent = (req, res) => {
-  const { columnId, nextColumnContent } = req.body;
+  const columnId = req.body.column_id;
+  const nextColumnContent = req.body.next_column_content;
   if (isValid(columnId, nextColumnContent)) {
     useDbConnection(async (con) => {
       const [[selectResult]] = await con.query(selectQuery, [columnId]);
+      if (!selectResult) {
+        res.status(400);
+        res.json({ error: "Invalid values" });
+        return;
+      }
       const prevColumnContent = selectResult.content;
 
       const [updateResult] = await con.query(updateQuery, [
         nextColumnContent,
         columnId,
       ]);
-      if (updateResult.changedRows === 1) {
-        res.json({
-          result: {
-            log_id: 361,
-            prev_column_content: prevColumnContent,
-            next_column_content: nextColumnContent,
-          },
-        });
-      } else {
-        res.status(500);
-        res.json({ error: "수정된 내용 없음." });
-      }
+      res.json({
+        result: {
+          log_id: 361,
+          prev_column_content: prevColumnContent,
+          next_column_content: nextColumnContent,
+        },
+      });
     });
   } else {
     res.status(400);
