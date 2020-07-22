2774d5fb84006ad1e9a46a3cd3073ffad82b5f4c
Atobaum <atobaum@gmail.com>
2020-07-22 16:32:41 +0900

    feat: 초기화 시 로그 불러오기 #93

--- client/src/components/Menu/index.js
+++ client/src/components/Menu/index.js
@@ -1,6 +1,7 @@
 import { Element, List } from "../basic";
 import Log from "../Log";
 import "./Menu.css";
+import api from "../../lib/apiCallWrapper";
 
 export default class Menu extends Element {
   constructor() {
@@ -14,9 +15,12 @@ export default class Menu extends Element {
     this.appendChild(activityTitle);
     this.$logs = new List(true);
     this.appendChild(this.$logs);
-    this.$logs.push(1, new Log({ todoContent: "로그1" }));
-    this.$logs.push(2, new Log({ todoContent: "로그2" }));
-    this.$logs.push(3, new Log({ todoContent: "로그3" }));
+    api.loadLogsApi().then((res) => {
+      console.log(res);
+      res.forEach(({ action_type, data }) => {
+        this.log(action_type, data);
+      });
+    });
   }
 
   log(type, data) {
