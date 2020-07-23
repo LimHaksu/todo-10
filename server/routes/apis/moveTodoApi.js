eb90a0687b48c5f73b3106c30e8ef198408a6a23
Atobaum <atobaum@gmail.com>
2020-07-23 14:30:03 +0900

    feat: Todo 옮기는 api 구현 #14
    다른 컬럼으로 옮기면 로그를 반환하고 같은 컬럼 내에서는 null을 줍니다.
    db의 todo.idx를 1,2,3... 로 유지합니다.
    단, 요청시 next_idx를 올바르게 요청해야합니다.

--- server/routes/apis/moveTodoApi.js
+++ server/routes/apis/moveTodoApi.js
@@ -1,9 +1,18 @@
 import useDbConnection from "../../lib/useDbConnection";
+import saveLog from "../../lib/saveLog";
+
+const selectTodoSql = "SELECT * FROM todo WHERE id=? AND user_id=?";
+const updatePrevColumnSql =
+  "UPDATE todo SET idx=idx-1 WHERE idx>? AND column_id=?";
+const updateNextColumnSql =
+  "UPDATE todo SET idx=idx+1 WHERE idx>=? AND column_id=?";
+const updateTodoSql = "UPDATE todo SET idx=?, column_id=? WHERE id=?";
+const selectColumnSql = "SELECT * FROM todo_column WHERE id=?";
 
 export default (req, res) => {
   useDbConnection(async (conn) => {
     const user = req.user;
-    if (!req.user) {
+    if (!user) {
       res.status(401);
       res.json({ error: "Invalid user" });
       return;
@@ -15,13 +24,37 @@ export default (req, res) => {
       "next_column_id",
     ].map((key) => parseInt(req.body[key]));
 
-    if ([todo_id, next_idx, next_column_id].some((n) => isNaN)) {
-      res.satus(400);
+    if ([todo_id, next_idx, next_column_id].some((n) => isNaN(n))) {
+      res.status(400);
       res.json({ error: "Invalid values" });
       return;
     }
 
-    const result = {};
-    res.json({ result });
+    const [[prevTodo]] = await conn.query(selectTodoSql, [todo_id, user.id]);
+    const [[prevColumn]] = await conn.query(selectColumnSql, [
+      prevTodo.column_id,
+    ]);
+    const [[nextColumn]] = await conn.query(selectColumnSql, [next_column_id]);
+    if (!(prevTodo && prevColumn && nextColumn)) {
+      res.status(400);
+      res.json({ error: "Invalid values" });
+      return;
+    }
+
+    await conn.query(updatePrevColumnSql, [prevTodo.idx, prevTodo.column_id]);
+    await conn.query(updateNextColumnSql, [next_idx, next_column_id]);
+    await conn.query(updateTodoSql, [next_idx, next_column_id, prevTodo.id]);
+
+    if (prevTodo.column_id === next_column_id) {
+      res.json({ result: null });
+    } else {
+      const log = await saveLog(conn, user.id, "todo_move", {
+        todoContent: prevTodo.content,
+        prevColumnContent: prevColumn.content,
+        nextColumnContent: nextColumn.content,
+      });
+
+      res.json({ result: log });
+    }
   });
 };
