ebab48e4b4a772cf9182d22d1f337ea4d348f1c7
Atobaum <atobaum@gmail.com>
2020-07-23 23:10:50 +0900

    feat: Create CreateColumnBox

---
+++ client/src/components/Kanban/CreateColumnBox.js
@@ -0,0 +1,38 @@
+import { Element, Modal, Input } from "../basic";
+import FormModal from "../FormModal";
+import "./CreateColumnBox.css";
+
+class CreateColumnModal extends Modal {
+  constructor({ onEdit }) {
+    const contentElement = new Element("div");
+    super(`Add a Column`, contentElement);
+
+    this.$input = new Input();
+
+    this.$form = new FormModal(
+      { inputType: "input", initialContent: "" },
+      onEdit
+    );
+
+    contentElement.appendChild(this.$form);
+  }
+}
+
+export default class CreateColumnBox extends Element {
+  constructor() {
+    super("div", {
+      class: ["create-column-box", "margin1"],
+      text: "Add a Column",
+    });
+
+    const createColumn = () => {
+      new CreateColumnModal({
+        onEdit: (content) => {
+          alert(content);
+        },
+      });
+    };
+
+    this.addEventListener("click", createColumn);
+  }
+}
